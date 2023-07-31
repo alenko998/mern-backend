f6e10a1cb8fecbc5bdd1a5bec44015ba6b3bdc25
alenko998 <alen_git@hotmail.com>
2023-07-31 15:44:21 +0200

    connect

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const express = require('express');
-const workoutRoutes = require('./routes/Workouts')
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/Workouts');
 
 // express app
 const app = express();
@@ -16,7 +17,15 @@ app.use((req,res,next) => {
 // routes
 app.use('/api/workouts',workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT,()=>{
-    console.log('listening on port 4000');
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+      // listen for requests
+      app.listen(process.env.PORT,()=>{
+      console.log('connected to db and listening on port ' , process.env.PORT);
+      })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
